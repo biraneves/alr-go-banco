c3ce07dc6662775156acb5ce7b137ea1eda6aff1
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-25 12:18:26 -0300

    Primeira versão do código, usando variáveis simples.

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var titular string = "Ubirajara Neves"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 125.50
+
+	fmt.Println("Titular:", titular)
+	fmt.Println("Agência:", numeroAgencia)
+	fmt.Println("Conta:", numeroConta)
+	fmt.Println("Saldo:",saldo)
+
+}
